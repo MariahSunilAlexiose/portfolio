588b6a040da88828c1522aa76d5c0abedfa83b71
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-08-14 20:45:03 -0400

    Added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,36 +7,47 @@ import { useTheme } from "@/providers"
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme()
-  // eslint-disable-next-line no-unused-vars
   const toggleTheme = () => {
     setTheme(theme === dark ? light : dark)
   }
 
   return (
-    <header className="bg-background">
-      <nav
-        className="mx-auto flex max-w-7xl items-center justify-between px-6 lg:px-8"
-        aria-label="Global"
-      >
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
-            <Image
-              className="h-8 w-auto"
-              src="/favicon.ico"
-              alt="Vercel Icon"
-              width={10}
-              height={10}
-            />
-          </a>
-        </div>
+    <header className="px-4 md:px-8 xl:px-20">
+      <nav className="my-8 flex items-center bg-foreground rounded-full justify-between px-6 py-2 md:px-14 md:py-[14px] lg:px-15 lg:py-[17.5px] xl:px-16 xl:py-7">
+        <a href="/">
+          <Image
+            src={
+              theme === dark
+                ? "/assets/icons/logo-black.svg"
+                : "/assets/icons/logo-white.svg"
+            }
+            alt="My Portfolio Logo"
+            width={62}
+            height={12}
+            className="w-16 h-3 md:w-[77px] md:h-5 lg:w-28 lg:h-6 xl:w-[152px] xl:h-8"
+          />
+        </a>
 
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-4">
+        <div className="flex gap-7 items-center">
           <a
-            href="#"
-            className="text-sm font-semibold leading-6 text-foreground"
+            href="/about-me"
+            className="text-xs md:text-sm lg:text-base xl:text-xl font-bold text-background font-sf_display"
           >
             About Me
           </a>
+          <button onClick={toggleTheme}>
+            <Image
+              src={
+                theme === dark
+                  ? "/assets/icons/sun-black.svg"
+                  : "/assets/icons/moon-white.svg"
+              }
+              alt="My Portfolio Logo"
+              className="w-4 md:w-5 lg:w-6 xl:w-8"
+              width={15}
+              height={15}
+            />
+          </button>
         </div>
       </nav>
     </header>
