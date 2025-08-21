bc2bc68c1d247237e98cf54df619e5582e35fab4
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-08-21 16:03:58 -0400

    Added projects section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Header } from "@/sections"
+import { Header, Projects } from "@/sections"
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Projects />
     </main>
   )
 }
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+
+import { Cards } from "@/containers"
+import { fetchData } from "@/scripts/useFetchData"
+import { ProjectType } from "@/types"
+
+type Props = {}
+
+const Projects = ({}: Props) => {
+  const [projects, setProjects] = useState<ProjectType[]>([])
+  useEffect(() => {
+    const fetchOptions = async () => {
+      const projectsData = await fetchData<ProjectType[]>("projects")
+      setProjects(projectsData)
+    }
+    fetchOptions()
+  }, [])
+  return (
+    <div className="flex flex-col gap-9">
+      <h2>My Projects</h2>
+      <Cards data={projects} />
+    </div>
+  )
+}
+
+export default Projects
