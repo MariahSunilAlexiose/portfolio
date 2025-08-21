df4c7502bb519ba1d743bc4571b07a6e213468a8
Mariah Sunil Alexiose <mariah.sunilalexiose@gmail.com>
2025-08-21 19:30:44 -0400

    Removed unnecessary code from Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,34 +1,11 @@
-"use client"
-
-import { useRouter } from "next/navigation"
-
-import { Button } from "@/components"
-
 const NotFound = () => {
-  const history = useRouter()
   return (
-    <div className="grid min-h-full place-items-center bg-background px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-primary text-base font-semibold">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Page not found
-        </h1>
-        <p className="mt-6 text-base leading-7 text-foreground/60">
-          Sorry, we could not find the page you are looking for.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Button
-            onClick={() => {
-              history.push("/")
-            }}
-          >
-            Go back home
-          </Button>
-          <a href="#" className="text-sm font-semibold text-foreground">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </div>
+    <div className="flex flex-col items-center gap-10 px-6 py-24 sm:py-32 lg:px-8">
+      <h2 className="text-primary font-semibold">404</h2>
+      <h1>Page not found</h1>
+      <h2 className="font-normal">
+        Sorry, we could not find the page you are looking for.
+      </h2>
     </div>
   )
 }
